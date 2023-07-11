584a1e08fd91eaee65462ea5f55045dfea58359c
sun.yiqi <sun.yiqi@kiroru-inc.jp>
2023-07-11 15:32:16 +0900

    チュートリアル - Material Design

--- app/src/main/java/com/example/tutorial_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/tutorial_jetpackcompose/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.tutorial_jetpackcompose
 
-import android.graphics.drawable.shapes.Shape
+import android.content.res.Configuration
+import android.graphics.Bitmap.Config
 import android.os.Bundle
-import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -10,13 +10,14 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,14 +25,21 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.tutorial_jetpackcompose.ui.theme.Tutorial_jetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Add a text element
-            // Text("Hello world!")
-            MessageCard(Message("子在川上曰", "逝者如斯夫，不舍昼夜。"))
+            // プロジェクト内で作成されたマテリアルテーマ
+            Tutorial_jetpackcomposeTheme {
+                // 関数ラップ用
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    // Add a text element
+                    // Text("Hello world!")
+                    MessageCard(Message("子在川上曰", "逝者如斯夫，不舍昼夜。"))
+                }
+            }
         }
     }
 }
@@ -52,6 +60,8 @@ fun MessageCard(msg: Message) {
                 .size(40.dp)
                 // Clip image to be shaped as round corner
                 .clip(RoundedCornerShape(10.dp))
+                // ボーダーを追加
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, RoundedCornerShape(10.dp))
         )
 
         // 画像とコロムの間にスペースを入れる
@@ -59,15 +69,42 @@ fun MessageCard(msg: Message) {
 
         // 列を使用する
         Column {
-            Text(text = msg.author)
-            Text(text = msg.body)
+            Text(
+                text = msg.author,
+                // 文字色
+                color = MaterialTheme.colorScheme.secondary,
+                // タイポグラフィスタイル
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            // shapeでラップしたい
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
 
 // Preview your function in Android Studio
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(msg = Message("李白", "飞流直下三千尺，疑是银河落九天。"))
+    // プロジェクト内で作成されたマテリアルテーマ
+    Tutorial_jetpackcomposeTheme {
+        // 関数ラップ用
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MessageCard(msg = Message("李白", "飞流直下三千尺，疑是银河落九天。"))
+        }
+    }
 }
